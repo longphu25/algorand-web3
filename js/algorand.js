7b8beca6a9a3e27f8889b68eb3b175f2b8126383
Long Phu <longphu257@gmail.com>
2024-06-13 23:01:07 +0700

    Init Module - Algorand Web3

---
+++ js/algorand.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Algorand behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.algorandAlgorand = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
